107376510c577649dda4758fd7cc176f715a842b
Alabhya Goel <jsr.alabhya@sihoresearch.com>
2026-01-21 08:53:49 +0530

    added service to keep render running

---
+++ App/renderUpService.py
@@ -0,0 +1,12 @@
+
+
+
+def renderUpService():
+    render = 'this service is use to keep the '
+    ser = 'render keep running'
+
+    final = render + ser
+
+
+if __name__=='__main__':
+    renderUpService()
